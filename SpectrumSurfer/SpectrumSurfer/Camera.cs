dfcb0b2050ad45a2c349275ea5471a160c59e28d
Randy1005 <randychang.10@gmail.com>
2020-08-29 00:15:19 -0600

    Camera Implemention Problems

---
+++ SpectrumSurfer/SpectrumSurfer/Camera.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using tainicom.Aether.Physics2D.Dynamics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using tainicom.Aether.Physics2D.Fluids;
+using System.Diagnostics;
+
+namespace SpectrumSurfer
+{
+    public class Camera
+    {
+        
+        public Matrix Transform
+        {
+            get;
+            private set;
+        }
+
+        public void Follow(Body target)
+        {
+            var offset = Matrix.CreateTranslation(Game1.ScreenWidth / 2f, Game1.ScreenHeight / 2f, 0f);
+            var position = Matrix.CreateTranslation(
+                -target.Position.X,
+                -target.Position.Y,
+                0f);
+
+            var zoom = Matrix.CreateScale(1.5f, 1.5f, 1f);
+
+
+            Transform = position * offset * zoom;
+        }
+    }
+}
--- SpectrumSurfer/SpectrumSurfer/Game1.cs
+++ SpectrumSurfer/SpectrumSurfer/Game1.cs
@@ -13,10 +13,15 @@ namespace SpectrumSurfer
 {
     public class Game1 : Game
     {
+
+        private Camera _camera;
+
+        public static int ScreenWidth;
+        public static int ScreenHeight;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private BasicEffect _spriteBatchEffect;
-        private SpriteFont _font;
         private bool isWaving = false;
 
         private Texture2D _playerTexture;
@@ -26,18 +31,22 @@ namespace SpectrumSurfer
         private Vector2 _playerTextureOrigin;
         private Vector2 _groundTextureOrigin;
 
+
+        public static float playerSpeed = 0.06f;
+
         private KeyboardState _oldKeyState;
+        
 
         // Simple camera controls
-        private Vector3 _cameraPosition = new Vector3(0, 1.70f, 0); // camera is 1.7 meters above the ground
-        float cameraViewWidth = 12.5f; // camera is 12.5 meters wide.
+        //private Vector3 _cameraPosition = new Vector3(0, 1.70f, 0); // camera is 1.7 meters above the ground
+        //float cameraViewWidth = 12.5f; // camera is 12.5 meters wide.
 
 
         // physics
         private World _world;
-        private Body _playerBody;
+        public static Body _playerBody;
         private Body _groundBody;
-        private float _playerBodyRadius = 0.7f / 2f; // player diameter is 1.5 meters
+        public static Vector2 _playerBodySize = new Vector2(0.7f, 0.7f); // 0.7 x 0.7
         private Vector2 _groundBodySize = new Vector2(15f, 1f); // ground is 15x1 meters
         private tainicom.Aether.Physics2D.Common.Vector2 _basePlayerPosition;
 
@@ -49,10 +58,7 @@ namespace SpectrumSurfer
             IsMouseVisible = true;
 
 
-            // stuff on the demo project, don't know the meaning yet
             _graphics.GraphicsProfile = GraphicsProfile.Reach;
-            _graphics.PreferredBackBufferWidth = 800;
-            _graphics.PreferredBackBufferHeight = 480;
             _graphics.IsFullScreen = false;
             _graphics.ApplyChanges();
 
@@ -63,14 +69,19 @@ namespace SpectrumSurfer
 
             base.Initialize();
 
+
+            // initialize screen width / height
+            ScreenHeight = _graphics.PreferredBackBufferHeight;
+            ScreenWidth = _graphics.PreferredBackBufferWidth;
+
             // Create a world
             _world = new World();
 
             /* Circle */
-            tainicom.Aether.Physics2D.Common.Vector2 playerPosition = new tainicom.Aether.Physics2D.Common.Vector2(0, _playerBodyRadius);
+            tainicom.Aether.Physics2D.Common.Vector2 playerPosition = new tainicom.Aether.Physics2D.Common.Vector2(0, _playerBodySize.X);
 
             // Create the player fixture
-            _playerBody = _world.CreateCircle(_playerBodyRadius, 1f, playerPosition);
+            _playerBody = _world.CreateRectangle(_playerBodySize.X, _playerBodySize.Y, 1f, playerPosition);
             _playerBody.BodyType = BodyType.Dynamic;
 
             // Give it some bounce and friction
@@ -94,11 +105,10 @@ namespace SpectrumSurfer
 
             _spriteBatch = new SpriteBatch(_graphics.GraphicsDevice);
 
-            // We use a BasicEffect to pass our view/projection in _spriteBatch
-            _spriteBatchEffect = new BasicEffect(_graphics.GraphicsDevice);
-            _spriteBatchEffect.TextureEnabled = true;
+            // load camera
+            _camera = new Camera();
 
-            //_font = Content.Load<SpriteFont>("font");
+
 
             // Load sprites
             _playerTexture = Content.Load<Texture2D>("CircleSprite");
@@ -122,16 +132,18 @@ namespace SpectrumSurfer
             // We update the world
             _world.Step(totalSeconds);
 
-            Debug.WriteLine(_playerBody.Position);
 
-            if (isWaving)
-            {
-                _playerBody.Position =
-                    new tainicom.Aether.Physics2D.Common.Vector2(
-                        _playerBody.Position.X + 0.03f,
-                        _basePlayerPosition.Y + 0.5f + (-(float)Math.Cos(_playerBody.Position.X * 3f) * 0.5f)
-                        );
-            }
+            //// just a test of sine wave
+            //if (isWaving)
+            //{
+            //    _playerBody.Position =
+            //        new tainicom.Aether.Physics2D.Common.Vector2(
+            //            _playerBody.Position.X + 0.03f,
+            //            _basePlayerPosition.Y + 0.5f + (-(float)Math.Cos(_playerBody.Position.X * 3f) * 0.5f)
+            //            );
+            //}
+
+            _camera.Follow(_playerBody);
 
             base.Update(gameTime);
         }
@@ -142,14 +154,14 @@ namespace SpectrumSurfer
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // Update camera View and Projection.
-            var vp = GraphicsDevice.Viewport;
-            _spriteBatchEffect.View = Matrix.CreateLookAt(_cameraPosition, _cameraPosition + Vector3.Forward, Vector3.Up);
-            _spriteBatchEffect.Projection = Matrix.CreateOrthographic(cameraViewWidth, cameraViewWidth / vp.AspectRatio, 0f, -1f);
+            //var vp = GraphicsDevice.Viewport;
+            //_spriteBatchEffect.View = Matrix.CreateLookAt(_cameraPosition, _cameraPosition + Vector3.Forward, Vector3.Up);
+            //_spriteBatchEffect.Projection = Matrix.CreateOrthographic(cameraViewWidth, cameraViewWidth / vp.AspectRatio, 0f, -1f);
 
             // Draw player and ground. 
             // Our View/Projection requires RasterizerState.CullClockwise and SpriteEffects.FlipVertically.
-            _spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, RasterizerState.CullClockwise, _spriteBatchEffect);
-            _spriteBatch.Draw(_playerTexture, new Vector2(_playerBody.Position.X, _playerBody.Position.Y), null, Color.White, _playerBody.Rotation, _playerTextureOrigin, new Vector2(_playerBodyRadius * 2f) / _playerTextureSize, SpriteEffects.FlipVertically, 0f);
+            _spriteBatch.Begin(transformMatrix: _camera.Transform);
+            _spriteBatch.Draw(_playerTexture, new Vector2(_playerBody.Position.X, _playerBody.Position.Y), null, Color.White, _playerBody.Rotation, _playerTextureOrigin, _playerBodySize / _playerTextureSize, SpriteEffects.FlipVertically, 0f);
             _spriteBatch.Draw(_groundTexture, new Vector2(_groundBody.Position.X, _groundBody.Position.Y), null, Color.White, _groundBody.Rotation, _groundTextureOrigin, _groundBodySize / _groundTextureSize, SpriteEffects.FlipVertically, 0f);
             _spriteBatch.End();
 
@@ -167,25 +179,25 @@ namespace SpectrumSurfer
             var vp = GraphicsDevice.Viewport;
 
             // Move camera
-            if (state.IsKeyDown(Keys.Left))
-                _cameraPosition.X -= totalSeconds * cameraViewWidth;
+            //if (state.IsKeyDown(Keys.Left))
+            //    _cameraPosition.X -= totalSeconds * cameraViewWidth;
 
-            if (state.IsKeyDown(Keys.Right))
-                _cameraPosition.X += totalSeconds * cameraViewWidth;
+            //if (state.IsKeyDown(Keys.Right))
+            //    _cameraPosition.X += totalSeconds * cameraViewWidth;
 
-            if (state.IsKeyDown(Keys.Up))
-                _cameraPosition.Y += totalSeconds * cameraViewWidth;
+            //if (state.IsKeyDown(Keys.Up))
+            //    _cameraPosition.Y += totalSeconds * cameraViewWidth;
 
-            if (state.IsKeyDown(Keys.Down))
-                _cameraPosition.Y -= totalSeconds * cameraViewWidth;
+            //if (state.IsKeyDown(Keys.Down))
+            //    _cameraPosition.Y -= totalSeconds * cameraViewWidth;
 
 
-            // We make it possible to rotate the player body
+            // We make it possible to move the player left and right
             if (state.IsKeyDown(Keys.A))
-                _playerBody.ApplyTorque(10);
+                _playerBody.Position = new tainicom.Aether.Physics2D.Common.Vector2(_playerBody.Position.X - playerSpeed, _playerBody.Position.Y);
 
             if (state.IsKeyDown(Keys.D))
-                _playerBody.ApplyTorque(-10);
+                _playerBody.Position = new tainicom.Aether.Physics2D.Common.Vector2(_playerBody.Position.X + playerSpeed, _playerBody.Position.Y);
 
             if (state.IsKeyDown(Keys.Space) && _oldKeyState.IsKeyUp(Keys.Space)) 
             {
